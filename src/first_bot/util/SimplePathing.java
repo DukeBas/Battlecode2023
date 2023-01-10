f4019d12dc4b3af6baf425f07a3dfa49cc666f23
DukeBas <35384117+DukeBas@users.noreply.github.com>
2023-01-10 13:49:49 +0100

    Somewhat less simple pathing

--- src/first_bot/util/SimplePathing.java
+++ src/first_bot/util/SimplePathing.java
@@ -1,6 +1,7 @@
 package first_bot.util;
 
 import battlecode.common.Direction;
+import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
@@ -15,9 +16,35 @@ public class SimplePathing implements Pathfinding {
     }
 
     @Override
-    public Direction getDirection(final MapLocation target) {
+    public Direction getDirection(final MapLocation target) throws GameActionException {
         MapLocation ownLocation = rc.getLocation();
+
+        // Best direct location to target
         Direction dirToTarget = ownLocation.directionTo(target);
-        return dirToTarget;
+
+        // Check if we can actually move to that spot
+        MapLocation toMoveTo = ownLocation.add(dirToTarget);
+
+        if (rc.sensePassability(toMoveTo) && !rc.isLocationOccupied(toMoveTo)){
+            return dirToTarget;
+        }
+
+        // Location was not possible... try out other options...
+        MapLocation[] options = new MapLocation[]{
+                ownLocation.add(dirToTarget.rotateLeft()),
+                ownLocation.add(dirToTarget.rotateRight()),
+                ownLocation.add(dirToTarget.rotateLeft().rotateLeft()),
+                ownLocation.add(dirToTarget.rotateRight().rotateRight()),
+        };
+
+        // Try to move to one of the alternative options
+        for (MapLocation loc : options) {
+            if (rc.sensePassability(loc) && !rc.isLocationOccupied(loc)){
+                return ownLocation.directionTo(loc);
+            }
+        }
+
+        // We cannot move closer so just wait?
+        return Direction.CENTER;
     }
 }
