8aae610f827af48ad98f6fe5f63bd3db4e1fa11b
DukeBas <35384117+DukeBas@users.noreply.github.com>
2023-01-15 14:55:56 +0100

    Copy old version, update to 1.3.0

---
+++ src/old_v2_presprint1/bots/Launcher.java
@@ -0,0 +1,100 @@
+package old_v2_presprint1.bots;
+
+import battlecode.common.*;
+
+public class Launcher extends Robot {
+
+    MapLocation Hq_target = null;
+    Boolean squad = false;
+    int HQ_id;
+
+    public Launcher(RobotController rc) throws GameActionException {
+        super(rc);
+        HQ_id = get_HQ_id(built_by);
+    }
+
+    /**
+     * This code is run once per turn (assuming we do not go over bytecode limits.)
+     *
+     * @throws GameActionException if an illegal game action is performed.
+     */
+    @Override
+    void run() throws GameActionException {
+        attack();
+
+        rc.setIndicatorString("tes " + rc.isActionReady());
+
+        // Only move if there is nothing to attack
+        if (rc.isActionReady()) {
+            if (get_number_of_launchers() > 1 || squad) {
+                squad = true;
+                if (Hq_target == null) {
+                    Hq_target = get_nearest_enemy_HQ();
+                }
+                if (Hq_target != null) {
+                    rc.setIndicatorString("Heading towards enemy HQ at " + Hq_target);
+                    move_towards(Hq_target);
+                }
+            }
+
+            if (rc.isMovementReady()) {
+                rc.setIndicatorString("Could find target, very sad");
+                int x = built_by.x < rc.getMapWidth() / 2 ? built_by.x + 2 : built_by.x - 2;
+                int y = built_by.y < rc.getMapHeight() / 2 ? built_by.y + 2 : built_by.y - 2;
+                rc.setIndicatorString(x + " " + y);
+                move_towards(new MapLocation(x, y));
+            }
+
+            // Try to attack again now that we've moved
+            if (rc.isActionReady()) {
+                attack();
+            }
+        }
+
+        scan();
+    }
+
+    public int get_number_of_launchers() throws GameActionException {
+        int count = 0;
+        RobotInfo[] robots = rc.senseNearbyRobots(5, friendly);
+        for (RobotInfo robot : robots) {
+            if (robot.getType() == RobotType.LAUNCHER) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void attack() throws GameActionException {
+        // Try to attack someone
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+
+//        // Sort enemies by hp
+//        Arrays.sort(enemies, Comparator.comparingInt(o -> o.health));
+
+        if (enemies.length > 0) {
+            RobotInfo toAttack = enemies[0];
+            int lowestHP = Integer.MAX_VALUE; // needs separate value as initial target might be HQ
+
+            for (RobotInfo r : enemies) {
+                RobotType type = r.getType();
+                if (type == RobotType.HEADQUARTERS) continue;
+
+                if ((type == toAttack.type && r.getHealth() < lowestHP) ||
+                        (type == RobotType.LAUNCHER && toAttack.type != RobotType.LAUNCHER) ||
+                        toAttack.getType() == RobotType.HEADQUARTERS) {
+                    lowestHP = r.getHealth();
+                    toAttack = r;
+                }
+            }
+
+            MapLocation loc = toAttack.getLocation();
+            if (rc.canAttack(loc)) {
+                rc.setIndicatorString("Attacking");
+                rc.attack(loc);
+            }
+        }
+    }
+}
