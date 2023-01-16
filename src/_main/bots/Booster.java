459ad62e6f7a02c6515ddfecfc5528a02b7b0e9c
omqriko <omar200273@gmail.com>
2023-01-16 15:49:05 +0100

    added initial booster code

--- src/_main/bots/Booster.java
+++ src/_main/bots/Booster.java
@@ -1,6 +1,9 @@
 package _main.bots;
 
 import battlecode.common.*;
+
+import java.util.HashSet;
+
 import _main.util.Constants;
 
 public class Booster extends Robot{
@@ -16,5 +19,52 @@ public class Booster extends Robot{
     @Override
     void run() throws GameActionException {
         scan();
+
+        // Run away from enemy launchers!
+        RobotInfo[] enemies = rc.senseNearbyRobots(RobotType.LAUNCHER.actionRadiusSquared, enemy);
+        int enemy_launcher = 0;
+        for (RobotInfo r : enemies) {
+            if (r.getType() == RobotType.LAUNCHER) {
+                enemy_launcher++;
+            }
+        }
+
+        if (enemy_launcher > 0) {
+            MapLocation enemy = enemies[0].getLocation();
+            Direction dir = combatPathing.tryDirection(rc.getLocation().directionTo(enemy).opposite());
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+            }
+        } else {
+            // Move towards the most friendly launchers
+            move_towards_launchers();
+        }
+
+        if (rc.canBoost()) {
+            rc.boost();
+        }
+    }
+
+    public void move_towards_launchers() throws GameActionException {
+        HashSet<Direction> launcher_directions = new HashSet<Direction>();
+        RobotInfo[] friendlies = rc.senseNearbyRobots(-1, enemy);
+        for (RobotInfo r : friendlies) {
+            launcher_directions.add(rc.getLocation().directionTo(r.getLocation()));
+        }
+        
+        // Try all directions, choose one with maximal directions
+        Direction best = Direction.CENTER;
+        int max_launchers = Integer.MIN_VALUE;
+        for (Direction l_loc : launcher_directions) {
+            int count = rc.senseNearbyRobots(rc.getLocation().add(l_loc), RobotType.LAUNCHER.actionRadiusSquared, friendly).length;
+            if (count > max_launchers) {
+                best = l_loc;
+                max_launchers = count;
+            }
+        }
+
+        if (rc.canMove(best)) {
+            rc.move(best);
+        }
     }
 }
