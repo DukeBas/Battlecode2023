267e34a4e6243db28e04a7740f74e6da14ef2ecd
DukeBas <35384117+DukeBas@users.noreply.github.com>
2023-01-16 15:56:28 +0100

    Checkpointing v5

---
+++ src/old_v5/RobotPlayer.java
@@ -0,0 +1,50 @@
+package old_v5;
+
+import battlecode.common.*;
+import old_v5.bots.*;
+
+
+/**
+ * RobotPlayer is the class that describes your main robot strategy.
+ * The run() method inside this class is like your main function: this is what we'll call once your robot
+ * is created!
+ */
+public strictfp class RobotPlayer {
+    /**
+     * run() is the method that is called when a robot is instantiated in the Battlecode world.
+     * It is like the main function for your robot. If this method returns, the robot dies!
+     *
+     * @param rc The RobotController object. You use it to perform actions from this robot, and to get
+     *           information on its current status. Essentially your portal to interacting with the world.
+     **/
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+        // Depending on the type of the bot, create a robot of that type
+        final Robot robot;
+        switch (rc.getType()) {
+            case HEADQUARTERS:
+                robot = new HQ(rc);
+                break;
+            case CARRIER:
+                robot = new Carrier(rc);
+                break;
+            case LAUNCHER:
+                robot = new Launcher(rc);
+                break;
+            case BOOSTER:
+                robot = new Booster(rc);
+                break;
+            case DESTABILIZER:
+                robot = new Destabilizer(rc);
+                break;
+            case AMPLIFIER:
+                robot = new Amplifier(rc);
+                break;
+            default:
+                robot = null;
+                break;
+        }
+
+        robot.runGameLoop();
+    }
+}
---
+++ src/old_v5/util/BugPathing.java
@@ -0,0 +1,69 @@
+package old_v5.util;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+
+/**
+ * Bug pathfinding going straight to target, if most direct way is blocked, keep going to the right until we make it
+ */
+public class BugPathing implements Pathfinding {
+    RobotController rc;
+    int mapWidth;
+    int mapHeight;
+    boolean prefer_right = true;
+
+    public BugPathing(RobotController rc) {
+        this.rc = rc;
+        this.mapWidth = rc.getMapWidth();
+        this.mapHeight = rc.getMapWidth();
+    }
+
+    @Override
+    public Direction getDirection(final MapLocation target) throws GameActionException {
+        MapLocation ownLocation = rc.getLocation();
+
+        // Best direct location to target
+        Direction dirToTarget = ownLocation.directionTo(target);
+
+        // Check if we can actually move to that spot
+        MapLocation toMoveTo = ownLocation.add(dirToTarget);
+
+        if (rc.canSenseLocation(toMoveTo) && rc.sensePassability(toMoveTo) && !rc.isLocationOccupied(toMoveTo)) {
+            return dirToTarget;
+        }
+
+        // Location was not possible... try more and more to the right
+        MapLocation next;
+        if (prefer_right){
+            for (int i = 7; --i >= 0; ) {
+                dirToTarget = dirToTarget.rotateRight();
+                next = ownLocation.add(dirToTarget);
+
+                if (rc.canSenseLocation(next) && rc.sensePassability(next) && !rc.isLocationOccupied(next)) {
+                    return ownLocation.directionTo(next);
+                }
+            }
+        } else {
+            for (int i = 7; --i >= 0; ) {
+                dirToTarget = dirToTarget.rotateLeft();
+                next = ownLocation.add(dirToTarget);
+
+                if (rc.canSenseLocation(next) && rc.sensePassability(next) && !rc.isLocationOccupied(next)) {
+                    return ownLocation.directionTo(next);
+                }
+            }
+        }
+
+
+        // We cannot move closer so just wait?
+        rc.setIndicatorString("bug pathfinding done booboo, idk what do for target loc " + target);
+        return Direction.CENTER;
+    }
+
+    // Flips what way the bug pathing prefers
+    public void flipDirection(){
+        prefer_right = !prefer_right;
+    }
+}
